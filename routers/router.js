5461fdc4863f05d88596eefa196fb6e273f3612a
Bruno HAS <brunohas21@gmail.com>
2022-10-31 19:12:25 -0300

    update basic

--- routers/router.js
+++ routers/router.js
@@ -6,5 +6,9 @@ module.exports = (app, con) => {
     app.get("/home", (req, res) => {
         res.render("paginaPrincipal.ejs")
     })
+
+    app.get("/menu" , (req, res) => {
+        res.render("menuprincipal.ejs")
+    })
     
 }
